79f3c62ced742e783faa02471e6bed2b8b40d500
Marco Ackermann <ackermann.marco@gmail.com>
2022-05-18 09:03:48 +0200

    update starter

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import App from './App.svelte'
 
+import './styles/index.scss'
+
 const app = new App({
   target: document.getElementById('app')
 })
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,5 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  plugins: [svelte()],
-  fs: {
-    allow: ['..']
-  }
+  plugins: [svelte()]
 })
