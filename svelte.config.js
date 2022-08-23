c98a644fbff11be8c4d4ec30392e1f66c77f0841
Marco Ackermann <ackermann.marco@gmail.com>
2022-08-23 13:49:40 +0200

    update starter project

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,10 @@
+const { resolve } = require('path')
 const sveltePreprocess = require('svelte-preprocess')
 
 module.exports = {
-  preprocess: sveltePreprocess()
+  preprocess: sveltePreprocess({
+    scss: {
+      includePaths: [resolve('./src/styles')]
+    }
+  })
 }
